50c93dc4c02e0a1daf227826f73251b9b4e35052
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-01-22 20:17:55 -0800

    finished

--- hello.asm
+++ hello.asm
@@ -4,13 +4,42 @@
 #  Data Area - allocate and initialize variables
 .data
 	# TODO: Write your string definitions here
+    integer: .asciiz "Enter an integer:\n"
+    hello: .asciiz "Hello Pandemic World of 2021!\n"
+    number: .asciiz "The magic number is "
+    end: .asciiz "!!!\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
-
 	# TODO: Write your code here
+    li $v0, 4
+    la $a0, integer
+    syscall
+
+    li $v0, 5
+    syscall
+
+    move $t0, $v0
+
+    li $v0, 4
+    la $a0, hello
+    syscall
+
+    li $v0, 4
+    la $a0, number
+    syscall
+
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 4
+    la $a0, end
+    syscall
 
 exit:
 	# Exit SPIM: Write your code here!
+    li $v0, 10
+    syscall
 
